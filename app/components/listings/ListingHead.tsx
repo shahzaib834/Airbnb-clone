644867d31923b7a5049f535e869b42308640e4b4
shahzaib834 <virgo834@gmail.com>
2023-08-23 21:52:40 +0500

    resolved build issues

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,19 +1,18 @@
 'use client';
 
-import Image from "next/image";
+import Image from 'next/image';
 
-import useCountries from "@/app/hooks/useCountries";
-import { SafeUser } from "@/app/types";
+import useCountries from '@/app/hooks/useCountries';
 
-import Heading from "../Heading";
-import HeartButton from "../HeartButton";
+import Heading from '../Heading';
+import HeartButton from '../HeartButton';
 
 interface ListingHeadProps {
   title: string;
   locationValue: string;
   imageSrc: string;
   id: string;
-  currentUser?: SafeUser | null
+  currentUser?: any;
 }
 
 const ListingHead: React.FC<ListingHeadProps> = ({
@@ -21,47 +20,45 @@ const ListingHead: React.FC<ListingHeadProps> = ({
   locationValue,
   imageSrc,
   id,
-  currentUser
+  currentUser,
 }) => {
   const { getByValue } = useCountries();
 
   const location = getByValue(locationValue);
 
-  return ( 
+  return (
     <>
       <Heading
         title={title}
         subtitle={`${location?.region}, ${location?.label}`}
       />
-      <div className="
+      <div
+        className='
           w-full
           h-[60vh]
           overflow-hidden 
           rounded-xl
           relative
-        "
+        '
       >
         <Image
           src={imageSrc}
           fill
-          className="object-cover w-full"
-          alt="Image"
+          className='object-cover w-full'
+          alt='Image'
         />
         <div
-          className="
+          className='
             absolute
             top-5
             right-5
-          "
+          '
         >
-          <HeartButton 
-            listingId={id}
-            currentUser={currentUser}
-          />
+          <HeartButton listingId={id} currentUser={currentUser} />
         </div>
       </div>
     </>
-   );
-}
- 
+  );
+};
+
 export default ListingHead;
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,35 @@
-import getCurrentUser from "./actions/getCurrentUser";
-import Container from "./components/Container";
-import getListings, { 
-  IListingsParams
-} from "@/app/actions/getListings";
-import EmptyState from "./components/EmptyState";
-import ListingCard from "./components/listings/ListingCard";
+export const dynamic = 'force-dynamic';
+import getCurrentUser from './actions/getCurrentUser';
+import Container from './components/Container';
+import getListings, { IListingsParams } from '@/app/actions/getListings';
+import EmptyState from './components/EmptyState';
+import ListingCard from './components/listings/ListingCard';
 
 interface HomeProps {
-  searchParams: IListingsParams
+  searchParams: IListingsParams;
 }
 
-const Home = async ({searchParams}: HomeProps) => {
+const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
-  const currentUser = await getCurrentUser() || undefined;
+  const currentUser = (await getCurrentUser()) || undefined;
 
   if (listings.length === 0) {
-    return (
-        <EmptyState showReset />
-    );
+    return <EmptyState showReset />;
   }
 
   return (
     <Container>
-      <div className="pt-24 grid grid-col-1 sm:grip-col-2 md:grid-col-4 lg:grid-cols-5 xl:grid-cols-6 gap-8">
+      <div className='pt-24 grid grid-col-1 sm:grip-col-2 md:grid-col-4 lg:grid-cols-5 xl:grid-cols-6 gap-8'>
         {listings.map((listing: any) => (
-            <ListingCard
-              currentUser={currentUser}
-              key={listing.id}
-              data={listing}
-            />
-          ))}
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
       </div>
     </Container>
-  )
-}
+  );
+};
 
 export default Home;
